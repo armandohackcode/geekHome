258cf11d4d93768c93013d51ec23d2709756aa69
armandohackcode <jlopezrodriguez687@gmail.com>
2024-08-27 20:59:05 -0400

    Corrección de datos del Readme

--- lib/config/api/api_config_example.dart
+++ lib/config/api/api_config_example.dart
@@ -1,3 +1,9 @@
+/**
+ * Haga una copia del archivo
+ * cambie el nombre a api_config.dart
+ * añada sus credenciales de acceso
+ */
+
 /// BaseUrl dominio de la app
 const String baseUrl = "https://comicvine.gamespot.com/api/";
 
